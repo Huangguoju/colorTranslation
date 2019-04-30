25f826e2b1c5bd34425dd190ad5550681f073f29
Huangguoju <33600779+Huangguoju@users.noreply.github.com>
2019-04-30 10:40:46 +0800

    hugo
    can run

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,10 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include <QDragEnterEvent>
-#include <QDebug>
-#include <QGraphicsView>
-#include <QFileDialog>
-#include <QMessageBox>
+
 
 
 
@@ -12,7 +8,6 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
-    qDebug() << "sdgsss2222tt222222";
     ui->setupUi(this);
     init();
 }
@@ -22,34 +17,214 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
 void MainWindow::init()
 {
-    ui->comboBox->addItem("VIDEO_NORMAL", 0);
-    ui->comboBox->addItem("VIDEO_720P", 1);
-    ui->comboBox->addItem("VIDEO_720P_T", 2);
-    ui->comboBox->addItem("VIDEO_720P_C", 3);
-    ui->comboBox->addItem("VIDEO_1080P", 4);
-    ui->comboBox->addItem("VIDEO_1080P_T", 5);
-    ui->comboBox->addItem("VIDEO_1080P_C", 6);
-    ui->comboBox->addItem("VIDEO_3M_A", 7);
-    ui->comboBox->addItem("VIDEO_3M_T", 8);
+    ui->cameraFormatBox->addItem("VIDEO_NORMAL", 0);
+    ui->cameraFormatBox->addItem("VIDEO_720P", 1);
+    ui->cameraFormatBox->addItem("VIDEO_720P_T", 2);
+    ui->cameraFormatBox->addItem("VIDEO_720P_C", 3);
+    ui->cameraFormatBox->addItem("VIDEO_1080P", 4);
+    ui->cameraFormatBox->addItem("VIDEO_1080P_T", 5);
+    ui->cameraFormatBox->addItem("VIDEO_1080P_C", 6);
+    ui->cameraFormatBox->addItem("VIDEO_3M_A", 7);
+    ui->cameraFormatBox->addItem("VIDEO_3M_T", 8);
+    ui->cameraFormatBox->addItem("VIDEO_3M_C", 9);
+    ui->cameraFormatBox->addItem("VIDEO_QXGA_A", 10);
+    ui->cameraFormatBox->addItem("VIDEO_QXGA_T", 11);
+    ui->cameraFormatBox->addItem("VIDEO_4M_A", 12);
+    ui->cameraFormatBox->addItem("VIDEO_4M_T", 13);
+    ui->cameraFormatBox->addItem("VIDEO_4M_C", 14);
+    ui->cameraFormatBox->addItem("VIDEO_5M_A", 15);
+    ui->cameraFormatBox->addItem("VIDEO_5M_T", 16);
+    ui->cameraFormatBox->addItem("VIDEO_8M_A", 17);
+    ui->cameraFormatBox->addItem("VIDEO_8M_T", 18);
+    ui->cameraFormatBox->addItem("VIDEO_8M_C", 19);
+    ui->cameraFormatBox->setCurrentIndex(4);
 
-    QLine_UIdefault_list << ui->default_line1 << ui->default_line2 << ui->default_line3
-                            << ui->default_line4 << ui->default_line5;
+    ui->ADchipTypeBox->addItem("NORMAL", 1);
+    ui->ADchipTypeBox->addItem("Color_AHD_6124B", 2);
+    ui->ADchipTypeBox->addItem("Color_TVI_2823/2853", 3);
+    ui->ADchipTypeBox->addItem("Color_AHD_6134", 7);
+    ui->ADchipTypeBox->addItem("Color_AHD_6134C", 8);
+    ui->ADchipTypeBox->addItem("Color_TVI_2826", 9);
+    ui->ADchipTypeBox->addItem("Color_TVI_2827", 10);
+    ui->ADchipTypeBox->addItem("Color_TVI_2827C", 11);
+    ui->ADchipTypeBox->addItem("Color_AHD_6158", 12);
+    ui->ADchipTypeBox->addItem("Color_AHD_6158C", 13);
+    ui->ADchipTypeBox->addItem("Color_TVI_2829", 14);
+    ui->ADchipTypeBox->addItem("Color_TVI_2828", 15);
+    ui->ADchipTypeBox->setCurrentIndex(3);
+
     QLine_UIcolor_list << ui->UIcolor_line1 << ui->UIcolor_line2 << ui->UIcolor_line3
                             << ui->UIcolor_line4 << ui->UIcolor_line5;
     QLine_TRcolor_list << ui->TRcolor_line1 << ui->TRcolor_line2 << ui->TRcolor_line3
                             << ui->TRcolor_line4 << ui->TRcolor_line5;
-    QLine_Cameracolor_list << ui->CameraColor_line1 << ui->CameraColor_line2 << ui->CameraColor_line3
-                            << ui->CameraColor_line4 << ui->CameraColor_line5;
+
+    ui->UIDefaultDisplayVal->clear();
+    ui->CameraDefaultMapVal->clear();
 
 }
 
-void MainWindow::on_pushButton_clicked()
+int MainWindow::ReadColorSetting(const QSettings &config, const QString &key,
+                     RS_U8 *pColorSettting)
 {
-    qDebug() << "asdgsadgh";
-    //QFileDialog *file = new QFileDialog;
-    //file->Accept();
+    QString str = config.value(key).toString();
+    QString strTrimed;
+    for (int i = 0; i < str.size(); i++) {
+        if (str[i] == ',' || (str[i] >= '0' && str[i] <= '9'))
+            strTrimed.append(str[i]);
+    }
+
+    QStringList strList = strTrimed.split(',');
+    if (strList.size() == 5)
+    {
+        for(int i = 0; i < strList.size(); i++)
+        {
+            pColorSettting[i] = strList[i].toUInt();
+        }
+        return 0;
+    }
+    else
+    {
+        for(int i = 0; i < strList.size(); i++)
+        {
+            pColorSettting[i] = 0;
+        }
+        return -1;
+    }
+}
+
+void MainWindow::ConfigCameraColor(int chiptype)
+{
+    //QSettings config("custom-config.ini", QSettings::IniFormat);
+    QSettings config(filename, QSettings::IniFormat);
+    QString maindirect;
+    for(int i = 0; i < VIDEO_MAX; i++){
+        memset(CameraColorVal[i], 0, 5);
+    }
+
+    switch(chiptype)  {
+        case RS_AD_CHIP_TYPE_AHD:
+            maindirect = "Color_AHD_6124B";
+            break;
+        case RS_AD_CHIP_TYPE_AHD_6134C:
+            maindirect = "Color_AHD_6134C";
+            break;
+        case RS_AD_CHIP_TYPE_TVI:
+            maindirect = "Color_TVI_2823/2853";
+            break;
+        case RS_AD_CHIP_TYPE_AHD_6134:
+            maindirect = "Color_AHD_6134";
+            break;
+        case RS_AD_CHIP_TYPE_TVI_2826:
+            maindirect = "Color_TVI_2826";
+            break;
+        case RS_AD_CHIP_TYPE_TVI_2827:
+            maindirect = "Color_TVI_2827";
+            break;
+        case RS_AD_CHIP_TYPE_TVI_2827C:
+            maindirect = "Color_TVI_2827C";
+            break;
+        case RS_AD_CHIP_TYPE_AHD_6158:
+            maindirect = "Color_AHD_6158";
+            break;
+        case RS_AD_CHIP_TYPE_AHD_6158C:
+            maindirect = "Color_AHD_6158C";
+            break;
+        case RS_AD_CHIP_TYPE_TVI_2829:
+            maindirect = "Color_TVI_2829";
+            break;
+        case RS_AD_CHIP_TYPE_TVI_2828:
+            maindirect = "Color_TVI_2828";
+            break;
+        default:
+            maindirect = "Color_NORMAL";
+            break;
+    }
+
+    ReadColorSetting(config, maindirect + "/NormalColorDefaultVal", CameraColorVal[VIDEO_NORMAL]);
+    ReadColorSetting(config, maindirect + "/AHD720PColorDefaultVal", CameraColorVal[VIDEO_720P]);
+    ReadColorSetting(config, maindirect + "/AHD1080PColorDefaultVal", CameraColorVal[VIDEO_1080P]);
+    ReadColorSetting(config, maindirect + "/AHD300WColorDefaultVal", CameraColorVal[VIDEO_QXGA_A]);
+    ReadColorSetting(config, maindirect + "/AHD400WColorDefaultVal", CameraColorVal[VIDEO_4M_A]);
+    ReadColorSetting(config, maindirect + "/AHD500WColorDefaultVal", CameraColorVal[VIDEO_5M_A]);
+    ReadColorSetting(config, maindirect + "/AHD800WColorDefaultVal", CameraColorVal[VIDEO_8M_A]);
+    ReadColorSetting(config, maindirect + "/TVI720PColorDefaultVal", CameraColorVal[VIDEO_720P_T]);
+    ReadColorSetting(config, maindirect + "/TVI1080PColorDefaultVal", CameraColorVal[VIDEO_1080P_T]);
+    ReadColorSetting(config, maindirect + "/TVI300WColorDefaultVal", CameraColorVal[VIDEO_3M_T]);
+    ReadColorSetting(config, maindirect + "/TVI400WColorDefaultVal", CameraColorVal[VIDEO_4M_T]);
+    ReadColorSetting(config, maindirect + "/TVI500WColorDefaultVal", CameraColorVal[VIDEO_5M_T]);
+    ReadColorSetting(config, maindirect + "/TVI800WColorDefaultVal", CameraColorVal[VIDEO_8M_T]);
+    ReadColorSetting(config, maindirect + "/CVI720PColorDefaultVal", CameraColorVal[VIDEO_720P_C]);
+    ReadColorSetting(config, maindirect + "/CVI1080PColorDefaultVal", CameraColorVal[VIDEO_1080P_C]);
+    ReadColorSetting(config, maindirect + "/CVI300WColorDefaultVal", CameraColorVal[VIDEO_3M_C]);
+    ReadColorSetting(config, maindirect + "/CVI400WColorDefaultVal", CameraColorVal[VIDEO_4M_C]);
+    ReadColorSetting(config, maindirect + "/CVI800WColorDefaultVal", CameraColorVal[VIDEO_8M_C]);
+
+    ReadColorSetting(config, "DisplaySenceDef/UIColorDefaultVal", UIColorDefaultVal);
+}
+
+void MainWindow::on_openFile_clicked()
+{
+
+    filename = QFileDialog::getOpenFileName(this,tr("open custom-config.ini"),
+                                            QDir::currentPath(), tr("configuration files (*.ini)"));
+
+    if (!filename.isEmpty()) {
+        if (ui->directoryComboBox->findText(filename) == -1)
+            ui->directoryComboBox->addItem(filename);
+        ui->directoryComboBox->setCurrentIndex(ui->directoryComboBox->findText(filename));
+    }
+
+    QFile file(filename);
+    if(!file.open(QFile::ReadOnly | QFile::Text))
+        return;
+
+//    QTextStream in(&file);
+//    while (!in.atEnd()) {
+//        QString line = in.readLine();
+//        qDebug() << line ;
+//    }
+    QSettings config(filename, QSettings::IniFormat);
+//    RS_U8 UIColorDefaultVal[5];
+//    ReadColorSetting(config, "DisplaySenceDef/UIColorDefaultVal", UIColorDefaultVal);
+//    qDebug() << UIColorDefaultVal[0] << UIColorDefaultVal[1];
+
+    ColorMode = config.value("DisplaySenceDef/ColorMode").toUInt();
+
+    QString m_DefaultDisplayVal = config.value("DisplaySenceDef/UIColorDefaultVal").toString();
+    ui->UIDefaultDisplayVal->setText(m_DefaultDisplayVal);
+
+    doRefreshCameraDefaultMapVal();
+}
+
+void MainWindow::doRefreshCameraDefaultMapVal()
+{
+    int m_ADType = ui->ADchipTypeBox->itemData(ui->ADchipTypeBox->currentIndex()).toInt();
+
+    ConfigCameraColor(m_ADType);
+
+    int m_videoFormat = ui->cameraFormatBox->currentIndex();
+
+    char CameraTmp[16];
+    sprintf(CameraTmp, "hbcss{%d, %d, %d, %d, %d}", CameraColorVal[m_videoFormat][0], CameraColorVal[m_videoFormat][1],\
+            CameraColorVal[m_videoFormat][2],CameraColorVal[m_videoFormat][3],CameraColorVal[m_videoFormat][4]);
+
+    ui->CameraDefaultMapVal->setText(QString(CameraTmp));
+
+    char UIColorTmp[16];
+    sprintf(UIColorTmp, "%d", UIColorDefaultVal[0]);
+    ui->UIcolor_line1->setText(QString(UIColorTmp));
+    sprintf(UIColorTmp, "%d", UIColorDefaultVal[1]);
+    ui->UIcolor_line2->setText(QString(UIColorTmp));
+    sprintf(UIColorTmp, "%d", UIColorDefaultVal[2]);
+    ui->UIcolor_line3->setText(QString(UIColorTmp));
+    sprintf(UIColorTmp, "%d", UIColorDefaultVal[3]);
+    ui->UIcolor_line4->setText(QString(UIColorTmp));
+    sprintf(UIColorTmp, "%d", UIColorDefaultVal[4]);
+    ui->UIcolor_line5->setText(QString(UIColorTmp));
+
 }
 
 void MainWindow::on_commandLinkButton_clicked()
@@ -59,43 +234,38 @@ void MainWindow::on_commandLinkButton_clicked()
     {
         RS_S32 uiColorVal = QLine_UIcolor_list[i]->text().toInt();
         RS_S32 colorVal = translate(i, uiColorVal);
-        qDebug() << "colorVal:" << colorVal;
+        //qDebug() << "colorVal:" << colorVal;
         QString TRcolor = QString("%1").arg(colorVal);
         QLine_TRcolor_list[i]->setText(TRcolor);
-
-
     }
-
-
 }
 
 RS_S32 MainWindow::translate(int colorType,RS_S32 uiColorVal)
 {
-#if 1
+
     RS_S32 maxColorVal = 63;
     RS_S32 colorVal = 0;
-    RS_S32 UIColorDefaultVal = QLine_UIdefault_list[colorType]->text().toInt();
-    RS_S32 CameraColorVal = QLine_Cameracolor_list[colorType]->text().toInt();
-    //if(cfg_get_colorMode() == 1)
+
+    int chnvideofmt = ui->cameraFormatBox->currentIndex();
+
+    if(ColorMode == 1)
         maxColorVal = 255;
 
     if(uiColorVal >= 0 && uiColorVal <= maxColorVal)
     {
 
-
-
-        if(uiColorVal == UIColorDefaultVal)
+        if(uiColorVal == UIColorDefaultVal[colorType])
         {
-            colorVal = CameraColorVal;
+            colorVal = CameraColorVal[chnvideofmt][colorType];
         }
-        else if(uiColorVal > UIColorDefaultVal)
+        else if(uiColorVal > UIColorDefaultVal[colorType])
         {
-            colorVal = (int)((uiColorVal*(maxColorVal - CameraColorVal) + maxColorVal*(CameraColorVal - \
-                UIColorDefaultVal))/(maxColorVal - UIColorDefaultVal));
+            colorVal = (int)((uiColorVal*(maxColorVal - CameraColorVal[chnvideofmt][colorType]) + maxColorVal*(CameraColorVal[chnvideofmt][colorType] - \
+                UIColorDefaultVal[colorType]))/(maxColorVal - UIColorDefaultVal[colorType]));
         }
-        else if(uiColorVal < UIColorDefaultVal)
+        else if(uiColorVal < UIColorDefaultVal[colorType])
         {
-            colorVal = (int)((uiColorVal*CameraColorVal)/UIColorDefaultVal);
+            colorVal = (int)((uiColorVal*CameraColorVal[chnvideofmt][colorType])/UIColorDefaultVal[colorType]);
         }
 
     }
@@ -103,20 +273,23 @@ RS_S32 MainWindow::translate(int colorType,RS_S32 uiColorVal)
     {
         QMessageBox msgBox;
         msgBox.setText("uiColorVal is err!");
-        //msgBox.setInformativeText("Do you want to save your changes?");
-        //msgBox.setStandardButtons(QMessageBox::Save | QMessageBox::Discard | QMessageBox::Cancel);
-        //msgBox.setDefaultButton(QMessageBox::Save);
         msgBox.exec();
-        //DEBUG_INFO(("uiColorVal is err!\n"));
         return maxColorVal;
     }
+    char tmp[256];
+    sprintf(tmp, "############ colorType:%d %0x  uicolorval:%d colorval:%d\n",colorType,chnvideofmt,uiColorVal,colorVal);
+    qDebug() << tmp;
     return colorVal;
 
-   #endif
 }
 
 
+void MainWindow::on_cameraFormatBox_currentIndexChanged(int index)
+{
+    doRefreshCameraDefaultMapVal();
+}
 
-
-
-
+void MainWindow::on_ADchipTypeBox_currentIndexChanged(int index)
+{
+    doRefreshCameraDefaultMapVal();
+}
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,12 @@
 
 #include <QMainWindow>
 #include <QLineEdit>
+#include <QDragEnterEvent>
+#include <QDebug>
+#include <QGraphicsView>
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QSettings>
 
 namespace Ui {
 class MainWindow;
@@ -29,6 +35,52 @@ typedef enum
     COLOR_SHARPNESS_E = 4
 }COLOR_TYPE_E;
 
+/* 前端AD 芯片的类型(AHD, TVI, ...)*/
+typedef enum _RS_AD_CHIP_TYPE_E
+{
+    RS_AD_CHIP_TYPE_NORMAL = 1,
+    RS_AD_CHIP_TYPE_AHD,
+    RS_AD_CHIP_TYPE_TVI,
+    RS_AD_CHIP_TYPE_AHD_TVI,
+    RS_AD_CHIP_TYPE_PR1000,
+    RS_AD_CHIP_TYPE_AHD_6134B,
+    RS_AD_CHIP_TYPE_AHD_6134,
+    RS_AD_CHIP_TYPE_AHD_6134C,
+    RS_AD_CHIP_TYPE_TVI_2826,
+    RS_AD_CHIP_TYPE_TVI_2827,
+    RS_AD_CHIP_TYPE_TVI_2827C,
+    RS_AD_CHIP_TYPE_AHD_6158,
+    RS_AD_CHIP_TYPE_AHD_6158C,
+    RS_AD_CHIP_TYPE_TVI_2829,
+    RS_AD_CHIP_TYPE_TVI_2828,
+    RS_AD_CHIP_TYPE_UNKNOW
+}RS_AD_CHIP_TYPE_E;
+
+typedef enum
+{
+    VIDEO_NORMAL = 0,
+    VIDEO_720P,
+    VIDEO_720P_T,
+    VIDEO_720P_C,
+    VIDEO_1080P,
+    VIDEO_1080P_T,
+    VIDEO_1080P_C,
+    VIDEO_3M_A,
+    VIDEO_3M_T,
+    VIDEO_3M_C,
+    VIDEO_QXGA_A,
+    VIDEO_QXGA_T,
+    VIDEO_4M_A,
+    VIDEO_4M_T,
+    VIDEO_4M_C,
+    VIDEO_5M_A,
+    VIDEO_5M_T,
+    VIDEO_8M_A,
+    VIDEO_8M_T,
+    VIDEO_8M_C,
+    VIDEO_MAX
+}AHDTVI_VIDEO_FORMAT_E;
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -39,18 +91,31 @@ public:
 
 
     RS_S32 translate(int colorType, RS_S32 uiColorVal);
+    int ReadColorSetting(const QSettings &config, const QString &key, RS_U8 *pColorSettting);
+    void ConfigCameraColor(int chiptype);
 private slots:
     void init();
-    void on_pushButton_clicked();
 
     void on_commandLinkButton_clicked();
 
+    void doRefreshCameraDefaultMapVal();
+
+    void on_openFile_clicked();
+
+    void on_cameraFormatBox_currentIndexChanged(int index);
+
+    void on_ADchipTypeBox_currentIndexChanged(int index);
+
 private:
     Ui::MainWindow *ui;
-    QList<QLineEdit *> QLine_UIdefault_list;
     QList<QLineEdit *> QLine_UIcolor_list;
     QList<QLineEdit *> QLine_TRcolor_list;
-    QList<QLineEdit *> QLine_Cameracolor_list;
+
+
+    RS_U8 CameraColorVal[VIDEO_MAX][5];
+    RS_U8 UIColorDefaultVal[5];
+    RS_U8 ColorMode;
+    QString filename;
 };
 
 #endif // MAINWINDOW_H
